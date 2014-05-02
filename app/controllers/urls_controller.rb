90ef4cd38942fb70a7a983fe45e3a8b60f0d010c
tigarcia <tim.m.garcia@gmail.com>
2014-05-02 09:55:35 -0700

    Implementing create method for urls and an empty show page

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -3,4 +3,19 @@ class UrlsController < ApplicationController
   def new
     @url = Url.new
   end
+
+  def create
+    url = Url.new url_params
+    url.random_string = SecureRandom.urlsafe_base64(6)
+    url.save
+    redirect_to url
+  end
+
+  def show
+  end
+
+  private
+    def url_params
+      params.require(:url).permit(:link)
+    end
 end
